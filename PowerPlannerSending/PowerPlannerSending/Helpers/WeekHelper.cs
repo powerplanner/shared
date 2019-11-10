bb1794c0aefe2f2d96dd2acb00fad915333bfcd1
Andrew Leader <followthatleader@outlook.com>
2019-11-10 08:58:02 -0800

    Add week helper

---
+++ PowerPlannerSending/PowerPlannerSending/Helpers/WeekHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerPlannerSending.Helpers
+{
+    public static class WeekHelper
+    {
+        /// <summary>
+        /// Assumes both values are UTC. Returns which week the specified date is on
+        /// </summary>
+        /// <param name="weekOneStartsOn"></param>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static Schedule.Week GetWeekOnDifferentDate(DateTime weekOneStartsOn, DateTime date)
+        {
+            int days;
+
+            if (date.Date >= weekOneStartsOn.Date)
+                days = (date.Date - weekOneStartsOn.Date).Days;
+
+            else
+                days = (weekOneStartsOn.Date - date.Date).Days + 6;
+
+            if (days / 7 % 2 == 0)
+            {
+                return Schedule.Week.WeekOne;
+            }
+
+            else
+            {
+                return Schedule.Week.WeekTwo;
+            }
+        }
+    }
+}
